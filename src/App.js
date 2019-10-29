6b94f43b9d6013a1f8e4d228e4db469e622405a2
argo46 <adityaargo4609@gmail.com>
2019-10-30 03:25:56 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import JobContents from './Components/JobContents'
+import Navbar from './Components/Navbar'
+const queryString = require('querystring')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import qs from 'qs'
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data:{},
+      next:'',
+      prev:'',
+      logo:'',
+      query:{qname:'', qcompany:'', orderby:''},
+      isLoading: true,
+      page:'http://localhost:3000/job/jobs/?'
+    }
+  }
+
+  async componentDidMount(){
+    this.getData()
+    .then(data => {
+      console.log(data)
+      this.setState({data,
+                    next:data.next_page,
+                    prev:data.prev_page,
+                    isLoading:false})
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+
+  getData = async (url) => {
+    if(url === undefined ){ 
+      const jobs = await axios.get(this.state.page + queryString.stringify(this.state.query))
+      console.log(`get ${this.state.page}`)
+      return jobs.data
+    } else {
+      console.log(`get ${url}`)
+      const jobs = await axios.get(url)
+      return jobs.data
+    }
+    
+  }
+  buttonPress = async (url) => {
+    this.setState({isLoading:true})
+    console.log(`url btn ${url}`)
+    this.getData(url)
+    .then(data => {
+      this.setState({data,
+                    next:data.next_page,
+                    prev:data.prev_page,
+                    isLoading: false })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+  goToDetail = (id) => {
+    // this.props.history.push('/character'+id)
+    // TODO : go to detail jobs
+  }
+
+  doSearch = async () => {
+    this.getData()
+      .then(data => {
+        this.setState({data,
+                      next:data.next_page,
+                      prev:data.prev_page,
+                      isLoading: false })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      console.log(`http://localhost:3000/job/jobs/?qname=${this.state.qname}&qcompany=${this.state.qcompany}`)
+  }
+  onChangeName = (event) => {
+    this.state.query.qname = event.target.value
+  }
+
+  onChangeCompany = (event) => {
+    this.state.query.qcompany = event.target.value
+  }
+
+  sortBy = (queryOrder) => {
+    console.log(`SORT ${queryOrder} = ${this.state.page}`)
+    this.state.query.orderby = queryOrder
+    // this.state.page = this.state.page + `orderby=${query}&order=asc`
+    this.getData()
+      .then(data => {
+        console.log(this.state.page)
+        this.setState({data,
+                      next:data.next_page,
+                      prev:data.prev_page,
+                      isLoading: false })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <JobContents 
+          state={this.state} 
+          buttonPress={this.buttonPress} 
+          doSearch={this.doSearch}
+          onChangeName={this.onChangeName}
+          onChangeCompany={this.onChangeCompany}
+          sortBy={this.sortBy}> 
+        </JobContents>
+      </div>
+      
+    )
+  }
 }
-
-export default App;
---
+++ src/Components/JobContents.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import {Row, Col, Container, 
+  Button, Input, InputGroup, 
+  InputGroupAddon, Dropdown, 
+  DropdownToggle, DropdownItem, DropdownMenu} from 'reactstrap'
+
+function JobContents (props) {
+
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
+
+  return (
+    <Container style={{margin:'1rem', maxWidth: '40rem'}}>
+      <Row style={{margin:'0'}}>
+        <InputGroup>
+          <Input type="search"
+                placeholder="Nama Pekerjaan"
+                onChange={props.onChangeName}
+                value={props.state.qname} />
+          <Input type="search"
+                placeholder="Nama Perusahaan"
+                onChange={props.onChangeCompany}
+                value={props.state.qcompany} />
+          <InputGroupAddon addonType="append">
+            <Button color="primary" onClick={()=>props.doSearch()}>Cari</Button>
+        </InputGroupAddon>
+      </InputGroup>
+      </Row>
+      <Row>
+        <div className='col-sm-6'>
+          <p style={{margin: '1rem 0 0 0'}}><strong>{props.state.data.total_result}</strong> pekerjaan ditemukan</p>
+        </div>
+        <div className='col-sm-6'>
+        <Dropdown isOpen={isOpen} toggle={toggle} style={{position:'absolute', bottom:'0', right:'0', marginRight: '1rem'}}>
+          <DropdownToggle className='btn btn-primary' caret style={{fontSize:'0.75rem'}}>
+          urutkan
+          </DropdownToggle>
+            
+          <DropdownMenu>
+            <DropdownItem onClick={()=>props.sortBy('date_updated')} >Tanggal diperbarui</DropdownItem>
+            <DropdownItem onClick={()=>props.sortBy('name')}>Nama Pekerjaan</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        </div>
+      </Row>
+      <Row className='justiy-content-md-center list-group' style={{padding:'0', margin: '0'}}>
+        {props.state.isLoading&&(
+          <p>Loading...</p>
+        )}
+        {!props.state.isLoading&&
+          <React.Fragment>
+            {props.state.data.result.map((v) => (
+              <div className='list-group-item'>
+              <Row className='row col-md-12' key={v.id}>
+                <div className='col-md-3'><img onClick={()=>this.goToDetail(v.id)} src={v.company_logo} alt='Company Logo' style={{display:'block', maxWidth: '110px'}}/></div>
+                <div className='col-md-9'>
+                  <p style={{fontWeight: 'bold'}}>{v.name}</p> 
+                  <p>{v.location} | {v.category}</p>
+                  <p>{v.salary}</p> 
+                  <p className='text-justify' style={{maxHeight: '150px', overflow:'hidden', textOverflow: 'ellipsis'}}>{v.description}</p>
+                </div>
+              </Row>
+              </div>
+            ))}
+          </React.Fragment>
+        }
+      </Row>
+      <Row className='justify-content-center'>
+        <Col>
+        {
+          props.state.prev === '' ? <Button color='primary' disabled>Prev</Button> :
+          <Button color='primary' onClick={()=>props.buttonPress(props.state.prev)}>Prev</Button>
+        }
+        <span>&nbsp;</span>
+        {
+          props.state.next === '' ? <Button color='primary' disabled>Next</Button> :
+          <Button color='primary' onClick={()=>props.buttonPress(props.state.next)}>Next</Button>
+        }
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default JobContents
